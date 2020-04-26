390b0a02a850d417bd3d6bd11cca7e9d15f988ce
Zouhair Guijjane <zh.guijjane@gmail.com>
2020-04-26 20:54:55 +0200

    [CLIENT] Setup Java module project

---
+++ Client/src/main/java/com/guijjane/konux/KonuxApplication.java
@@ -0,0 +1,13 @@
+package com.guijjane.konux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KonuxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KonuxApplication.class, args);
+    }
+
+}
